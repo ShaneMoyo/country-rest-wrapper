dc08a5bbbcc08c0535eb7dbd928ee6bb1f54fd39
shanemoyo <smoyo@pdx.edu>
2020-06-28 15:32:02 -0700

    add form.

--- src/components/Countries/index.js
+++ src/components/Countries/index.js
@@ -1,10 +1,30 @@
-import React from 'react'; 
+import React, { useState } from 'react'; 
 
 
 export default function CountriesContainer() { 
+
+    const [query, setQuery] = useState(""); 
+    const handleSubmit = event => { 
+        event.preventDefault(); 
+        console.log('query: ', query); 
+
+    }
+
     return (
         <section>
             <h1>Countries</h1> 
+            <form onSubmit={event => handleSubmit(event)}> 
+                <fieldset>
+                    <label htmlFor="country-search">Search for a country</label><br/>
+                    <input 
+                        id="country-search"
+                        type="text" 
+                        onChange={({ target }) => setQuery(target.value)} 
+                        value={query}
+                        placeholder="Enter a country name"/>
+                    <button type="submit">Search</button> 
+                </fieldset>
+            </form>
         </section>
     ); 
 }
