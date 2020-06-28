cc3aebd814ea4ab25b8a116351c87b5899a7ec6a
shanemoyo <smoyo@pdx.edu>
2020-06-28 16:48:29 -0700

    wip

--- src/components/Countries/country/index.js
+++ src/components/Countries/country/index.js
@@ -1,7 +1,40 @@
 import React, { useEffect, useState } from 'react'; 
+import { getCountryByName } from '../../../services/countryAPI';
 
-export default function Country() { 
-    return (
-        <h1>Country: </h1>
-    )
+export default function Country(props) { 
+    const [loading, setLoading ] = useState(true); 
+    const [error, setError] = useState(false); 
+    const [country, setCountry] = useState({}); 
+    const { country: countryName } = props.match.params; 
+    console.log('props: ', props, countryName); 
+
+    async function getCountry(name) { 
+        try { 
+            console.log('getting with: ', name);
+            const response = await getCountryByName(name); 
+            setLoading(false)
+            console.log('response in get country : ', response[0]); 
+            setCountry(response[0]); 
+        } catch (error) { 
+            console.log( 'error: ', error); 
+            setError(true); 
+            setLoading(false); 
+        }
+        
+    }
+    useEffect(() => { 
+        getCountry(countryName); 
+    }, [countryName]); 
+
+    if(loading) { 
+        return <h1>Loading...</h1>
+    } else if(error) { 
+        return <h1>Something went wrong...</h1>
+    } else { 
+        return (
+            <section>
+                <h1>Country: {country.name}</h1> 
+            </section>
+        )
+    }
 }
--- src/services/countryAPI.js
+++ src/services/countryAPI.js
@@ -8,3 +8,14 @@ export const getCountryByName = async country => {
         console.log('error: ', error); 
     }
 }
+
+export const getCountryByFullName = async country => { 
+    try { 
+        const response = await fetch(`https://restcountries.eu/rest/v2/name/${country}?fullText=true`); 
+        console.log('response: ', response);
+        if(!response.ok) throw new Error({ status: 500, message: 'Server error.'}); 
+        return response.json(); 
+    } catch (error) { 
+        console.log('error: ', error); 
+    }
+}
